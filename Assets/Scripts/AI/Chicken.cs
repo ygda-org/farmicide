17781d96b63e726258d39f354e0fa647d289a757
Almondbubby <79113528+Almondbubby@users.noreply.github.com>
2023-03-10 00:30:22 -0500

    make chicken detect player

--- Assets/Scripts/AI/Chicken.cs
+++ Assets/Scripts/AI/Chicken.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Chicken : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int target = 0;
+
+    public Rigidbody2D enemyRb;
+    public Transform enemyTrans;
+    public Transform player;
+    public float radius = 3;
+    bool playerDetected = false;
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 enemyPos = new Vector2(enemyTrans.position.x, enemyTrans.position.y);
+        RaycastHit2D ray = Physics2D.CircleCast(enemyPos, radius, new Vector2(0,0), 0);
+        if (ray.collider != null)
+        {
+            if (target == 0)
+            {
+                if (ray.collider.name == "Player1") playerDetected = true;
+            }
+            else if (target == 1)
+            {
+                if (ray.collider.name == "Player2") playerDetected = true;
+            }
+        }
+        else playerDetected = false;
+        if (playerDetected == true) Debug.Log("player detected");
     }
+
 }
