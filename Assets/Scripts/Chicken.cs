b93f6bf83cc0c59cf201eef27e50c1ea0f1ee49d
Claeb101 <calebarulandu@gmail.com>
2023-06-01 12:43:50 -0400

    chickens! they don't attack, just follow u around rn

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Chicken : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Target _target;
+    private GameObject target;
+
+    private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _target = GetComponent<Target>();
+        _rb = GetComponent<Rigidbody2D>();
+        target = _target.owner.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var dir = (target.transform.position - transform.position);
+        if (dir.magnitude > 2f)
+        {
+            _rb.velocity = dir.normalized * moveSpeed; // move toward
+        }
+        else if (dir.magnitude > 1f)
+        {
+            _rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            _rb.velocity = -dir.normalized * moveSpeed; // move away
+        }
     }
 }
